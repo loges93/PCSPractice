bc0d24868d679e547370d2d982cf030782bbca3e
loges93 <58162404+loges93@users.noreply.github.com>
2025-06-16 08:01:17 -0500

    Added form and pages to see data pipeline

--- PCSPractice/Controllers/AccountController.cs
+++ PCSPractice/Controllers/AccountController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PCSPractice.Data;
 
 namespace PCSPractice.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly PcsDbContext _context;
+
+        public AccountController(PcsDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Login(string username, string password)
+        {
+            ViewBag.username = username;
+            ViewBag.password = password;
             return View();
         }
     }
